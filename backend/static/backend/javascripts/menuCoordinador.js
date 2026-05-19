9422aaee505ce73e6d821ff411aa347fb9153897
MarkusGutierrez10 <markusgutierrez10@gmail.com>
2026-05-19 16:15:29 -0500

    se creo la vista menu coordinador y usuarios coordinador web

---
+++ backend/static/backend/javascripts/menuCoordinador.js
@@ -0,0 +1,42 @@
+function mostrarvistaInicioCoordinador() {
+    cursos = document.getElementById("mostrarCursosAdmin").style = "display: none;"    
+    modulo = document.getElementById("mostrarModulosAdmin").style = "display:none;"
+    usuario = document.getElementById("mostrarUsuarioCoordinador").style = "display: none;"
+    asistencia = document.getElementById("mostrarAsistenciaAdmin").style = "display: none;"
+    inicio = document.getElementById("mostrarInicioCoordinador").style = "display: block;"
+}
+
+
+
+function mostrarvistaUsuarioAdmin() {
+    cursos = document.getElementById("mostrarCursosAdmin").style = "display: none;"    
+    modulo = document.getElementById("mostrarModulosAdmin").style = "display:none;"
+    inicio = document.getElementById("mostrarInicioCoordinador").style = "display: none;"
+    asistencia = document.getElementById("mostrarAsistenciaAdmin").style = "display: none;"
+    usuario = document.getElementById("mostrarUsuarioCoordinador").style = "display: block;"
+}
+/*
+function mostrarvistaModulosAdmin() {
+    cursos = document.getElementById("mostrarCursosAdmin").style = "display: none;"
+    asistencia = document.getElementById("mostrarAsistenciaAdmin").style = "display: none;"
+    inicio = document.getElementById("mostrarInicioAdmin").style = "display: none;"
+    usuario = document.getElementById("mostrarUsuarioAdmin").style = "display: none;"
+    modulo = document.getElementById("mostrarModulosAdmin").style = "display:block;"
+}
+
+function mostrarvistaCursosAdmin() {
+    inicio = document.getElementById("mostrarInicioAdmin").style = "display: none;"
+    usuario = document.getElementById("mostrarUsuarioAdmin").style = "display: none;"
+    modulo = document.getElementById("mostrarModulosAdmin").style = "display:none;"
+    asistencia = document.getElementById("mostrarAsistenciaAdmin").style = "display: none;"
+    cursos = document.getElementById("mostrarCursosAdmin").style = "display: block;"
+}
+function mostrarvistaAsistenciaAdmin() {
+    inicio = document.getElementById("mostrarInicioAdmin").style = "display: none;"
+    usuario = document.getElementById("mostrarUsuarioAdmin").style = "display: none;"
+    modulo = document.getElementById("mostrarModulosAdmin").style = "display:none;"
+    cursos = document.getElementById("mostrarCursosAdmin").style = "display: none;"
+    asistencia = document.getElementById("mostrarAsistenciaAdmin").style = "display: block;"
+}*/
+
+
