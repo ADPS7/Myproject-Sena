853ade1dd366adfb3431a69dfbaad69019eadbc6
ADPS7 <perez.suareza1@gmail.com>
2026-06-03 11:29:01 -0500

    Mensaje

--- app/lib/presentation/view/asiststudent.dart
+++ app/lib/presentation/view/asiststudent.dart
@@ -17,7 +17,7 @@ class AttendanceScreen extends StatelessWidget {
     if (userId == null) throw Exception('Usuario no encontrado');
 
     final response = await http.get(
-      Uri.parse('http://10.2.130.26:5000/asistencias/$userId'),
+      Uri.parse('http://10.2.131.189:5000/asistencias/$userId'),
       headers: {'Content-Type': 'application/json'},
     );
 
